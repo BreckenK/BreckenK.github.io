e1998c29409a1e25152d8e2bc0f39cdd188752b9
Brecken Keller <breckenjkeller@gmail.com>
2020-10-24 13:26:09 -0400

    updating materials

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
@@ -21,7 +27,6 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    res.send('Hello World');
   });
 
 app.listen(port, () => {
